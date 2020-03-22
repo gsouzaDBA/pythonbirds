0ff1a141a9e0a2229e851cf59e986a6feb5520ed
gsouza.dba@gmail.com <Marte@14>
2020-03-22 20:05:16 -0300

    Criado e removido atributo dinamico do objeto pessoa.py

--- pythonbirds/OO/pessoa.py
+++ pythonbirds/OO/pessoa.py
@@ -19,4 +19,8 @@ if __name__ == '__main__':
     #print(luciano.filhos)
     for filho in luciano.filhos:
         print(filho.nome)
+    luciano.sobrenome = 'Ramalho'
+    del luciano.filhos
+    print(luciano.__dict__)
+    print(renzo.__dict__)
 
