229a471de81d9390c7805b8e83bae2575a8bb729
gsouza.dba@gmail.com <Marte@14>
2020-03-26 16:17:20 -0300

    Criado os métodos de Direção: Direita e Esquerda

--- pythonbirds/OO/carro.py
+++ pythonbirds/OO/carro.py
@@ -97,6 +97,40 @@ Exemplo:
 
 """
 
+NORTE='Norte'
+SUL='Sul'
+LESTE='Leste'
+OESTE='Oeste'
+
+class Direcao:
+    rotacao_a_direita_dct = {
+        NORTE: LESTE, LESTE: SUL, SUL: OESTE, OESTE: NORTE
+    }
+
+    rotacao_a_esquerda_dct = {
+        NORTE: OESTE, LESTE: NORTE, SUL: LESTE, OESTE: SUL
+    }
+
+    def __init__(self):
+        self.valor = NORTE
+
+    #def girar_a_direita(self):
+    #    if self.valor == NORTE:
+    #        self.valor = LESTE
+    #    elif self.valor == LESTE:
+    #        self.valor = SUL
+    #    elif self.valor == SUL:
+    #        self.valor = OESTE
+    # ...
+
+    #Código Melhorado usando o Dicionario, usado em solucões de chave valor
+    def girar_a_direita(self):
+        self.valor = self.rotacao_a_direita_dct[self.valor]
+
+    def girar_a_esquerda(self):
+        self.valor = self.rotacao_a_esquerda_dct[self.valor]
+
+
 class Motor:
     def __init__(self):
         self.velocidade = 0
